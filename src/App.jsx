98bce31109d1d0679a8e724bffaf0e3c52078613
kratiDatacode <krati.datacode@gmail.com>
2025-07-24 18:04:41 +0530

    Task krati : handle with useEffect instead of button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useDispatch,useSelector } from 'react-redux'
 import {fetchTodos} from "./redux/slice/todo";
 import './App.css'
 import TodoTable from './TodoTable';
+import { useEffect } from 'react';
 
 function App() {
   const dispatch = useDispatch();
@@ -9,14 +10,24 @@ function App() {
 
   console.log("State",state);
 
+   // 🔁 Call fetchTodos when the component mounts
+    useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
   if(state.todo.isLoading){
     return <h1>Loading.....</h1>;
   }
 
+  if (state.todo.isError) {
+    return <h1>Something went wrong!</h1>;
+  }
+
+
   return (
     <>
     <div className='App'>
-    <button onClick={(e)=>dispatch(fetchTodos())}>Fetch Todos</button>
+    {/* <button onClick={(e)=>dispatch(fetchTodos())}>Fetch Todos</button> */}
     {/* {
       state.todo.data && state.todo.data.map(e=><li>{e.title}</li>)
     } */}
